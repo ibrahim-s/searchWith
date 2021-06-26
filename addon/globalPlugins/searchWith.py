542a0b49390e487e5626951891d54b8de083ce81
ibrahim <ibra.hamadeh@hotmail.com>
2021-06-26 12:03:26 +0300

    Add setting panel, giving more language options in Google search

--- addon/globalPlugins/searchWith.py
+++ addon/globalPlugins/searchWith.py
@@ -2,11 +2,15 @@
 # NVDA Add-on: Search With
 # Copyright (C) 2021 ibrahim hamadeh
 # This add-on is free software, licensed under the terms of the GNU General Public License (version 2).
-# See the file copying for more details.
+# See the file COPYING for more details.
 
 import api
 import ui
+import gui, wx
+from gui import guiHelper
+import config
 import queueHandler
+import languageHandler
 import globalPluginHandler
 import textInfos
 import scriptHandler
@@ -41,29 +45,31 @@ engineNameAndUrl= (
 )
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
 	def __init__(self, *args, **kwargs):
 		super(GlobalPlugin, self).__init__(*args, **kwargs)
 		self.virtualMenuActive= False
-		#self.menuItems= ['Yahoo', 'Bing', 'DuckDuckGo', 'Youtube']
 		# Menu items, or labels of search engines in virtual menu.
-		self.menuItems= [key for key, value in engineNameAndUrl]
+		self.menuItems= [name for name, url in engineNameAndUrl]
 		self.index= 0
 
+		gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(SearchWithPanel)
+
+	def terminate(self):
+			gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(SearchWithPanel)
+
 	def activateMenu(self):
-		'''Activate virtual menu with its menu items.'''
-		log.info('activating virtual menu ...')
+		'''Display virtual menu with its menu items.'''
+		#log.info('activating virtual menu ...')
 		if self.virtualMenuActive:
 			return
 		log.info('binding gestures for virtual menu...')
 		for key in ('downArrow', 'upArrow', 'leftArrow', 'rightArrow'):
 			self.bindGesture(f'kb:{key}', 'moveOnVirtual')
-			#self.bindGesture('kb:downArrow', 'movingOnVirtual')
-			#self.bindGesture('kb:upArrow', 'movingOnVirtual')
-			self.bindGesture('kb:escape', 'closeVirtual')
+		self.bindGesture('kb:escape', 'closeVirtual')
 		self.bindGesture('kb:enter', 'activateMenuItem')
 		self.virtualMenuActive= True
-		#ui.message("Search With Menu {}".format(self.menuItems[self.index]))
-		queueHandler.queueFunction(queueHandler.eventQueue, ui.message, _("Search With Menu"))#
+		queueHandler.queueFunction(queueHandler.eventQueue, ui.message, _("Search With Menu"))
 		queueHandler.queueFunction(queueHandler.eventQueue, ui.message, f"{self.menuItems[self.index]}")
 			#log.info('Done binding')
 
@@ -75,17 +81,20 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			return
 		num= len(self.menuItems)
 		if key== 'downArrow':
-			self.index= self.index+1 if self.index!=3 else 0
+			self.index= self.index+1 if self.index!=num-1 else 0
 		elif key== 'upArrow':
 			self.index= self.index-1 if self.index!=0 else num-1
 		ui.message(f"{self.menuItems[self.index]}")
 
 	def script_closeVirtual(self, gesture):
+		''' Script to close the virtual menu.'''
 		self.clearVirtual()
 		# After destroying virtual menu, the focus object does not announce itself
+		# So we have to do that explicitly.
 		api.getFocusObject().reportFocus()
 
 	def clearVirtual(self):
+		''' Helper function to clear the virtual menu.'''
 		self.clearGestureBindings()
 		self.bindGestures(self.__gestures)
 		self.virtualMenuActive= False
@@ -96,39 +105,68 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		log.info('activating menu item ...')
 		text= isSelectedText()
 		index= self.index
-		"""if index== 0:
-			baseUrl= 'https://search.yahoo.com/search/?p='
-			webbrowser.open(baseUrl+ text)
-		elif index== 1:
-			baseUrl= 'https://www.bing.com/search?q='
-			webbrowser.open(baseUrl+ text)
-		elif index== 2:
-			baseUrl= 'https://duckduckgo.com/?q='
-			webbrowser.open(baseUrl+ text)
-		elif index== 3:
-			baseUrl= 'http://www.youtube.com/results?search_query='
-			webbrowser.open(baseUrl+ text)
-		"""
 		# Get the url of the search engine.
 		url= engineNameAndUrl[index][1]
 		webbrowser.open(url+ text)
 		self.clearVirtual()
 
-	def script_searchWitMenu(self, gesture):
+	def script_searchWith(self, gesture):
 		text= isSelectedText()
 		if not text:
 			# Translators: Message displayed if no text selected.
 			ui.message(_("No text selected"))
 			return
-		#url= 'https://google.com/search?q=%s'%text
 		scriptCount= scriptHandler.getLastScriptRepeatCount()
 		if scriptCount== 0:
 			self.activateMenu()
-		else:
-			googleUrl= 'https://google.com/search?q='
-			webbrowser.open(googleUrl+ text)
-			self.clearVirtual()
+			return
+		googleUrl= 'https://google.com/search?q='
+		if config.conf["searchWith"]["lang"]== 0:
+			lang= ""
+		elif config.conf["searchWith"]["lang"]== 1:
+			# NVDA language
+			lang= languageHandler.getLanguage()
+			lang= lang.split('_')[0] if '_' in lang else lang
+			log.info(f'nvdaLang: {lang}')
+		elif config.conf["searchWith"]["lang"]== 2:
+			# Windows language
+			lang= languageHandler.getWindowsLanguage()
+			lang= lang.split('_')[0] if '_' in lang else lang
+			log.info(f'winLang: {lang}')
+		langParam= f"&lr=lang_{lang}&hr={lang}" if lang else ""
+		webbrowser.open(googleUrl+ text+ langParam)
+		self.clearVirtual()
+
+	# Translators: Category of addon in input gestures.
+	script_searchWith.category= _("Search With")
+	# Translators: Message displayed in input help more.
+	script_searchWith.__doc__= _("Open search with menu if pressed once, search with Google directly twice.")
 
 	__gestures= {
-	'kb:nvda+e': 'searchWitMenu',
+	'kb:nvda+e': 'searchWith',
 	}
+
+#default configuration 
+configspec={
+	"lang": "integer(default=0)",
+}
+config.conf.spec["searchWith"]= configspec
+
+#make  SettingsPanel  class
+class SearchWithPanel(gui.SettingsPanel):
+	# Translators: title of the dialog
+	title= _("Search With")
+
+	def makeSettings(self, sizer):
+		settingsSizerHelper = guiHelper.BoxSizerHelper(self, sizer=sizer)
+
+		# Translators: Languages used in Google search.
+		langs= [_("Browser language and setting"), _("NVDA language"), _("Windows language")]
+		self.langsComboBox= settingsSizerHelper.addLabeledControl(
+		# Translators: label of cumbo box to choose language for Google search.
+		_("Choose language used in Google search"), 
+		wx.Choice, choices= langs)
+		self.langsComboBox.SetSelection(config.conf["searchWith"]["lang"])
+
+	def onSave(self):
+		config.conf["searchWith"]["lang"]= self.langsComboBox.GetSelection()
--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
 # Search With #
 
 Author: Ibrahim Hamadeh  
-Download [Development Version 0.1][1]  
+Download [Development Version 0.2][1]  
 NVDA compatibility: 2019.3 and beyond  
 
 This addon helps you to search selected text, via various search engines.  
 select the text and press once, a menu will be displayed with various search engines to choose from.  
 otherwise, you press the gesture twice to search with Google directly.  
-The Default gesture for the addon is: NVDA+ Alt+ M.  
+The Default gesture for the addon is: NVDA+ E.  
 but you can as always add a gesture or change the existed one going to :  
 NVDA menu>preferences>inputGestures>Search With.  
 
@@ -21,9 +21,20 @@ Yahoo, Bing, DuckDuckGo, and Youtube.
 *	Changed your mind? press the escape key to close the menu.  
 *	Or, if you want to search with Google directly  
 *	Press the gesture twice, and the results will be shown in the default browser.  
+*	Want to search Google using other language?  
+Go to, Search With category in setting panels  
+From the cumbo box there, you can choose search Google either:  
+	1.	Using browser settings and language.  
+	2.	Using NVDA language.  
+	3.	Using windows language.  
+That's it! Happy searching.  
 
-### Changes for 1.0 ###
+### Changes for 0.2 ###
+
+*	Add setting panel, giving more language options in Google search.  
+
+### Changes for 0.1 ###
 
 *	Initial version  
 
-[1]: https://github.com/ibrahim-s/searchWith/releases/download/0.1-dev/searchWith-0.1-dev.nvda-addon
+[1]: https://github.com/ibrahim-s/searchWith/releases/download/v0.2-dev/searchWith-0.2-dev.nvda-addon
